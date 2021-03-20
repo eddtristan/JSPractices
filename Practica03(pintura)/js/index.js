97aa9dc85e6f285cb2ab08eee410581812476c73
eddtristan <e.tristan.m@hotmail.com>
2021-03-19 19:13:10 -0600

    Estilos para la tienda de pintura

---
+++ Practica03(pintura)/js/index.js
@@ -0,0 +1,15 @@
+let btnCalcular = document.getElementById("btnCalcular");
+
+btnCalcular.addEventListener("click", calcularCosto);
+
+function calcularCosto() {
+    let costoMaterial = Number(document.getElementById("costoMaterial").value);
+    let altoPared = Number(document.getElementById("altoPared").value);
+    let anchoPared = Number(document.getElementById("anchoPared").value);
+
+    let resultado = costoMaterial * altoPared * anchoPared;
+
+    let containerResultado = document.getElementById("resultado");
+
+    containerResultado.innerHTML = `<em>El resultado es: </em> ${resultado}`;
+}
