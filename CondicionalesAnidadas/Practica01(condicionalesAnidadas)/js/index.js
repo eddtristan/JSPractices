bd3f8d0709048f7d05d6529e12a36b115e26731e
eddtristan <e.tristan.m@hotmail.com>
2021-04-07 18:03:51 -0500

    Comparando tres números

---
+++ CondicionalesAnidadas/Practica01(condicionalesAnidadas)/js/index.js
@@ -0,0 +1,13 @@
+const btnComparar = document.getElementById("btnComparar");
+
+btnComparar.addEventListener("click", compararNumeros);
+
+function compararNumeros() {
+    const inpNumero1 = Number(document.getElementById("inpNumber1").value);
+    const inpNumero2 = Number(document.getElementById("inpNumber2").value);
+    const inpNumero3 = Number(document.getElementById("inpNumber3").value);
+
+    const resultado = document.getElementById("resultado");
+
+    
+}
