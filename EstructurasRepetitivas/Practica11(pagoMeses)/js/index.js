2321e65b981ae7f140c669622f58ed6d423dd214
eddtristan <e.tristan.m@hotmail.com>
2021-04-18 18:11:27 -0500

    Mensualidad

--- EstructurasRepetitivas/Practica11(pagoMeses)/js/index.js
+++ EstructurasRepetitivas/Practica11(pagoMeses)/js/index.js
@@ -1,3 +1,22 @@
 const btnInitApp = document.getElementById("btnInitApp");
 
-btnInitApp.addEventListener("click", );
+btnInitApp.addEventListener("click", calcularMensualidad);
+
+function calcularMensualidad() {
+    let pagoInicial = 5;
+    let pagoMensual = 0;
+    let pagoTotal = 0;
+    let registro = "";
+
+    for (let i = 0; i < 20; i++) {
+        pagoMensual = pagoInicial * 2;
+        pagoInicial = pagoMensual;
+        pagoTotal = pagoTotal + pagoMensual;
+        registro += `<li class="list-group-item">El pago del mes ${i+1} es de $${pagoMensual}</li>`;
+    }
+
+    let resultContainer = document.getElementById("resultContainer");
+
+    resultContainer.innerHTML = `${registro}`;
+
+}
