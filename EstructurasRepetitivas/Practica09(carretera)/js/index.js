85a150b30d937b4f208ee824b6c92fb6cf4b7537
eddtristan <e.tristan.m@hotmail.com>
2021-04-18 17:27:09 -0500

    Punto de encuentro en carretera

--- EstructurasRepetitivas/Practica09(carretera)/js/index.js
+++ EstructurasRepetitivas/Practica09(carretera)/js/index.js
@@ -1,3 +1,19 @@
 const btnInitApp = document.getElementById("btnInitApp");
 
-btnInitApp.addEventListener("click", );
+btnInitApp.addEventListener("click", calcularIntercepcion);
+
+function calcularIntercepcion() {
+    let aZacatecas = 70;
+    let aAguascalientes = 150;
+    let distancia = aAguascalientes - aZacatecas;
+
+    while (distancia > 0) {
+        aZacatecas++;
+        aAguascalientes--;
+        distancia = aAguascalientes - aZacatecas;
+    }
+
+    let resultContainer = document.getElementById("resultContainer");
+
+    resultContainer.innerHTML = `<span>El punto de encuentro es el km ${aZacatecas}</span>`
+}
