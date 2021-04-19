b662483ce85379bc5e9a07c39943948d510ae0dc
eddtristan <e.tristan.m@hotmail.com>
2021-04-19 17:51:30 -0500

    Serie Fibonacci

--- EstructurasRepetitivas/Practica14(numeroFibonacci)/js/index.js
+++ EstructurasRepetitivas/Practica14(numeroFibonacci)/js/index.js
@@ -1,3 +1,34 @@
 const btnInitApp = document.getElementById("btnInitApp");
 
-btnInitApp.addEventListener("click", );
+btnInitApp.addEventListener("click", serieFibonacci);
+
+function serieFibonacci() {
+    let numero = Number(prompt("Ingrese el índex que desea obtener de la serie FIBONACCI"));
+    let contador = 0;
+    let auxiliar = 0;
+    let auxiliarDos = 1;
+    let numeroFibonacci = 0;
+    let continuar = true;
+    let serie = "";
+
+    if (numero < 0) {
+        alert("Ingrese un número positivo");
+        return;
+    }
+
+    while (continuar) {
+        if (contador == numero) {
+            continuar = false;
+        } else {
+            numeroFibonacci = auxiliar + auxiliarDos;
+            auxiliar = auxiliarDos;
+            auxiliarDos = numeroFibonacci;
+            serie += `<li class="list-group-item">${numeroFibonacci}</li>`;
+            contador++;
+        }
+    }
+
+    let resultContainer = document.getElementById("resultContainer");
+
+    resultContainer.innerHTML = `${serie}`;
+}
