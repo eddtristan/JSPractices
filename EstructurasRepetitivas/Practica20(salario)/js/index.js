668135bb7737df6119fa7f5d649a62721288f04e
eddtristan <e.tristan.m@hotmail.com>
2021-04-20 17:04:26 -0500

    Cálculo de salario

--- EstructurasRepetitivas/Practica20(salario)/js/index.js
+++ EstructurasRepetitivas/Practica20(salario)/js/index.js
@@ -1,3 +1,18 @@
 const btnInitApp = document.getElementById("btnInitApp");
 
-btnInitApp.addEventListener("click", );
+btnInitApp.addEventListener("click", calcularSalario);
+
+function calcularSalario() {
+    let salario = 1500;
+    let aumento = 1.10;
+    let aumentos = `<span>El salario inicial es de $1500</span><br>`;
+    
+    for (let i = 0; i < 6; i++) {
+        salario = salario * aumento;
+        aumentos += `<span>El incremento del salario en el año ${i+1} lo deja en $${Math.trunc(salario)}</span><br>`;
+    }
+
+    const resultContainer = document.getElementById("resultContainer");
+
+    resultContainer.innerHTML = `${aumentos}`;
+}
