6a97fb56e3102ace1a2b5b82e91cebbacb2379dc
eddtristan <e.tristan.m@hotmail.com>
2021-04-19 19:12:49 -0500

    Números primos

--- EstructurasRepetitivas/Practica16(primos)/js/index.js
+++ EstructurasRepetitivas/Practica16(primos)/js/index.js
@@ -1,3 +1,26 @@
 const btnInitApp = document.getElementById("btnInitApp");
 
-btnInitApp.addEventListener("click", );
+btnInitApp.addEventListener("click", revisarPrimos);
+
+function revisarPrimos() {
+    let numero = Number(prompt("Ingrese un número para encontrar la cantidad de números primos que tiene"));
+    let auxiliarDividendos = 0;
+    let contadorPrimos = 0;
+
+    for (let i = numero; i > 1; i--){
+        for (let k = numero; k > 0; k--){
+            if ((i % k) == 0) {
+                auxiliarDividendos++;
+            }
+        }
+        if (auxiliarDividendos < 3) {
+            contadorPrimos++;
+        }
+        auxiliarDividendos = 0;
+    }
+
+    let resultContainer = document.getElementById("resultContainer");
+
+    resultContainer.innerHTML = `<li class="list-group-item">La cantidad de números primos que hay menores a ${numero} es de ${contadorPrimos}</li>`;
+
+}
