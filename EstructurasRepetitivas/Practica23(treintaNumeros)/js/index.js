f98542476809e22a3b072878eb070bbad94af542
eddtristan <e.tristan.m@hotmail.com>
2021-04-21 12:01:31 -0500

    Promedio de impares y suma de pares

--- EstructurasRepetitivas/Practica23(treintaNumeros)/js/index.js
+++ EstructurasRepetitivas/Practica23(treintaNumeros)/js/index.js
@@ -1,3 +1,27 @@
 const btnInitApp = document.getElementById("btnInitApp");
 
-btnInitApp.addEventListener("click", );
+btnInitApp.addEventListener("click", leerNumeros);
+
+function leerNumeros() {
+    let suma = 0;
+    let promedio = 0;
+    let contadorPromedio = 0;
+    let numero = 0;
+
+    for (let i = 0; i < 30; i++) {
+        numero = Number(prompt(`Ingrese el número ${i+1} de 30`));
+        if (numero == 0) {
+            alert("Ingrese un número diferente de 0");
+            i--;
+        } else if ((numero % 2) == 0) {
+            promedio = promedio + numero;
+            contadorPromedio++;
+        } else {
+            suma = suma + numero;
+        }
+    }
+
+    const resultContainer = document.getElementById("resultContainer");
+
+    resultContainer.innerHTML = `<span>La suma de los números impares es de ${suma} y el promedio de los pares es de ${promedio / contadorPromedio}</span>`;
+}
